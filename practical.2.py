09303ae0327cf70b4062dc8a59ff32dba78178aa
2203031260022 <157503251+2203031260022@users.noreply.github.com>
2024-01-23 22:35:23 +0530

    Add files via upload

---
+++ practical.2.py
@@ -0,0 +1,8 @@
+# 2. A program that calculates the area and perimeter of a rectangle.
+fh=open("output2.txt",'w')
+l=int(input("enter length : "))
+w=int(input("enter width : "))
+area=l*w
+perimeter=2*(l+w)
+fh.write("area of rectangle is: "+str(area))
+fh.write("perimeter of rectangle is: "+str(perimeter))
